a405c5bb06eb5902eef67082aadf778d2cf915e4
fcc <fcc.felippe@gmail.com>
2018-04-06 12:00:48 -0300

    set 400 status code to invalid dv

--- app/controller.js
+++ app/controller.js
@@ -7,10 +7,13 @@ app.get('/:bolNumber', (req, res) => {
   const bolNumber = req.params.bolNumber
   try {
     let b = new Boleto(bolNumber).toObject()
+    if (!b.válido) {
+      res.status(400).json({'error': 'Número de Boleto Inválido'})
+    }
     res.status(200).json(b)
   }
   catch(err) {
-    res.status(400).json({'error': 'Invalid Input'})
+    res.status(400).json({'error': 'Input não é um número de boleto'})
   }
 })
 
